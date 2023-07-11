059181b392418a2fcbb29b15edc3fb2007786961
Alex Gonzales <gonzales.alexzander18@gmail.com>
2023-07-11 10:34:56 -0600

    finished exercise

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let button = document.querySelector("#submitSearch");
+let userInput = document.querySelector("#searchWord");
+let imageContainer = document.querySelector("img");
+let feedback = document.querySelector("#feedback");
+
+button.addEventListener("click",() => {
+    let giphyURL = `https://api.giphy.com/v1/gifs/translate?api_key=mxS5Wibw2baJOJP0MkwH2qheILCLKErm&s=${userInput.value}`;
+    fetch(giphyURL)
+    .then((result) => {
+        return result.json();
+    })
+    .then((data) => {
+        console.log(data);
+        imageContainer.src = data.data.images.original.url;
+        userInput.value = "";
+        feedback.textContent = "";
+
+    })
+    .catch((err) => {
+        console.error(err);
+        feedback.textContent = err;
+    })
+})
